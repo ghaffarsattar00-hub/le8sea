847dd37239a8f594cef30bd115890d3f54446a1b
ABDUL_GHAFFAR <ghaffarsattar00@gmail.com>
2026-07-12 00:39:50 +0500

    Add HTTPS enforcement in AppServiceProvider
    Enforce HTTPS scheme for non-local environments.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Yeh line add ki gayi hai
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Agar website local computer par nahi hai, toh HTTPS lazmi use kare
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
